9739bfa6e096455dfbd9149893b803d4588b3265
ferbncode <suyashgargsfam@gmail.com>
2016-01-09 14:40:56 +0530

    Fourth Commit

--- main.py
+++ main.py
@@ -10,6 +10,11 @@ class fintech(App):
     pass
 
 class List_screen(FloatLayout):
+    company = StringProperty()
+    l_fix = StringProperty()
+    l_cur = StringProperty()
+    Dep = StringProperty()
+
 
     def show_current_stocks(self,company):
         r = requests.get('http://finance.google.com/finance/info?client=ig&q=NSE:{}'.format(company))
@@ -24,18 +29,16 @@ class List_screen(FloatLayout):
         details = ast.literal_eval(details)
         self.clear_widgets()
         self.add_widget(stocksAtGlance())
-
-
+        self.company = details["t"]
+        self.l_fix = details["l_fix"]
+        self.l_cur = details["l_cur"]
+        self.Dep = details["e"]
 
 
 
 class showCurrentSituation(ListItemButton):
     pass
 class stocksAtGlance(FloatLayout):
-    company = StringProperty()
-    l_fix = StringProperty()
-    l_cur = StringProperty()
-    Dep = StringProperty()
     def fill_details(self):
         pass
     
